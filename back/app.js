79edf6000854eacaf450285213e37b829f35e49e
ClementH9 <henot.clement@gmail.com>
2023-10-16 21:30:21 +0200

    error next

--- back/app.js
+++ back/app.js
@@ -30,31 +30,31 @@ app.post('/api/sauce', (req, res, next) => {
     });
     sauce.save()
       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
+      .catch(error => res.status(400).json({ error }))
     });
 
 app.put('/api/sauce/:id', (req, res, next) => {
     Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-        .catch(error => res.status(400).json({ error }));
+        .catch(error => res.status(400).json({ error }))
     });
 
 app.delete('/api/sauce/:id', (req, res, next) => {
     Sauce.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-        .catch(error => res.status(400).json({ error }));
+        .catch(error => res.status(400).json({ error }))
     });
 
 app.get('/api/sauce/:id', (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
       .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(404).json({ error }));
+      .catch(error => res.status(404).json({ error }))
     });
 
-app.get('/api/sauces', (req, res, next) => {
+app.get('/api/sauce', (req, res, next) => {
     Sauce.find()
-    .then(sauce => res.status(200).json(sauce));
-    .catch(error => res.status(400).json({ error }));
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }))
     });
 
 module.exports = app;
