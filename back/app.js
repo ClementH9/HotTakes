beb5beae4f13cbcd21e3e7c8e72a5681241bcc49
ClementH9 <henot.clement@gmail.com>
2023-10-18 21:46:59 +0200

    routes models and controllers

--- back/app.js
+++ back/app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const bodyParser = require ('body-parser');
 const mongoose = require('mongoose');
 
-const Sauce = require ('./models/sauce');
+const sauceRoutes = require ('./routes/sauce');
 
 mongoose.connect('mongodb+srv://ClementH9:OpenClassrooms59@cluster0.k8lz0bp.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -23,38 +23,6 @@ app.use((req, res, next) => {
   
 app.use(bodyParser.json());
 
-app.post('/api/sauce', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce({
-      ...req.body
-    });
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }))
-    });
-
-app.put('/api/sauce/:id', (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-        .catch(error => res.status(400).json({ error }))
-    });
-
-app.delete('/api/sauce/:id', (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-        .catch(error => res.status(400).json({ error }))
-    });
-
-app.get('/api/sauce/:id', (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-      .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(404).json({ error }))
-    });
-
-app.get('/api/sauce', (req, res, next) => {
-    Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }))
-    });
+app.use('/api/sauces', sauceRoutes);
 
 module.exports = app;
