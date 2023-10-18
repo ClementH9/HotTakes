8a7cc849f49268e0eb398717679c04296b304dc4
ClementH9 <henot.clement@gmail.com>
2023-10-18 23:58:06 +0200

    token

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const User = require('..models/User');
 const user = require('../models/user');
@@ -30,8 +31,12 @@ exports.login = (req, res, next) => {
                 } else {
                     res.status(200).json({
                         userId: user._id,
-                        token: 'TOKEN'
-                    })
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
                 }
             })
             .catch(error => {
